558f9d897a8e3d90a6a6cfe9aa2de9c575416bc7
Bernio <jendabernard@gmail.com>
2026-07-26 19:30:02 +0200

    Načíst opravený přehrávač v PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vedator-temata-v25';
+const CACHE='vedator-temata-v26';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','audio-player.css','audio-player.js','catalog-patch.js'];
 
 self.addEventListener('install',event=>{
