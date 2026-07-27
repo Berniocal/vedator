97e782bffda0cd0144c6211ba404cdd0779e9e31
Bernio <jendabernard@gmail.com>
2026-07-27 09:16:50 +0200

    Deploy expandable episode descriptions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vedator-temata-v40';
-const VERSION='v40';
+const CACHE='vedator-temata-v41';
+const VERSION='v41';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','audio-player.css','audio-player.js','catalog-patch.js','custom-player.js','theme-toggle.js','ui-cleanup.js','highlight-patch.js'];
 
 self.addEventListener('install',event=>{
