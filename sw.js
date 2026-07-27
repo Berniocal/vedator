bf7bf89192476577df8b3f6bb4ea275b9ef29faa
Bernio <jendabernard@gmail.com>
2026-07-27 08:35:29 +0200

    Deploy contextual description excerpts

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vedator-temata-v39';
-const VERSION='v39';
+const CACHE='vedator-temata-v40';
+const VERSION='v40';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','audio-player.css','audio-player.js','catalog-patch.js','custom-player.js','theme-toggle.js','ui-cleanup.js','highlight-patch.js'];
 
 self.addEventListener('install',event=>{
