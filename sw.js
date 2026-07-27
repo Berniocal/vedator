b883eadd2165e07da8230e95444e202b758f8b66
Bernio <jendabernard@gmail.com>
2026-07-27 10:49:01 +0200

    Nasadit lokální zálohu dat aplikace v50

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='vedator-temata-v49';
-const VERSION='v49';
-const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','audio-player.css','audio-player.js','catalog-patch.js','custom-player.js','theme-toggle.js','ui-cleanup.js','highlight-patch.js','playlist-patch.js','slovak-topics-patch.js','topic-filter-fix.js','slovak-ui.js'];
+const CACHE='vedator-temata-v50';
+const VERSION='v50';
+const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','audio-player.css','audio-player.js','catalog-patch.js','custom-player.js','theme-toggle.js','ui-cleanup.js','highlight-patch.js','playlist-patch.js','slovak-topics-patch.js','topic-filter-fix.js','slovak-ui.js','data-backup.js'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
@@ -41,6 +41,7 @@ async function injectEnhancements(response){
   if(!html.includes('highlight-patch.js'))html=html.replace('</body>','<script src="./highlight-patch.js" defer></script></body>');
   if(!html.includes('playlist-patch.js'))html=html.replace('</body>','<script src="./playlist-patch.js" defer></script></body>');
   if(!html.includes('slovak-ui.js'))html=html.replace('</body>','<script src="./slovak-ui.js" defer></script></body>');
+  if(!html.includes('data-backup.js'))html=html.replace('</body>','<script src="./data-backup.js" defer></script></body>');
   if(!html.includes('__vedatorSwUpdater'))html=html.replace('</body>',updaterScript()+'</body>');
 
   const headers=new Headers(response.headers);
