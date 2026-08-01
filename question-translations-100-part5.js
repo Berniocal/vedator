8bda43b9c979ec717763c15b9ea4dbd4923c3ea1
Bernio <jendabernard@gmail.com>
2026-08-01 18:52:47 +0200

    Load episode 100 end and episode 89 translations

--- question-translations-100-part5.js
+++ question-translations-100-part5.js
@@ -103,4 +103,12 @@
   apply();
   new MutationObserver(schedule).observe(document.documentElement,{childList:true,subtree:true});
   window.addEventListener('vedatorlanguagechange',schedule);
+
+  if(!document.querySelector('script[data-vedator-translations-100-end-89-part1]')){
+    const script=document.createElement('script');
+    script.src='./question-translations-100-end-89-part1.js';
+    script.async=false;
+    script.dataset.vedatorTranslations100End89Part1='1';
+    document.head.appendChild(script);
+  }
 })();
