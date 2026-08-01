50e8acd34390548df1f53fe9c2ca49214ec46a04
Bernio <jendabernard@gmail.com>
2026-08-01 19:54:29 +0200

    Load next 12 reverse translations for episode 26

--- question-translations-35-end-26-part1.js
+++ question-translations-35-end-26-part1.js
@@ -112,4 +112,12 @@
   apply();
   new MutationObserver(schedule).observe(document.documentElement,{childList:true,subtree:true});
   window.addEventListener('vedatorlanguagechange',schedule);
+
+  if(!document.querySelector('script[data-vedator-translations-26-part2]')){
+    const script=document.createElement('script');
+    script.src='./question-translations-26-part2.js';
+    script.async=false;
+    script.dataset.vedatorTranslations26Part2='1';
+    document.head.appendChild(script);
+  }
 })();
