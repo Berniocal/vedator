60fb301b1037458458926cb5205784fa0492393f
Bernio <jendabernard@gmail.com>
2026-08-02 20:36:44 +0200

    Load Czech translations for episodes 35 to 28

--- episode-translations-loader.js
+++ episode-translations-loader.js
@@ -2,7 +2,7 @@
   if(window.__vedatorEpisodeTranslationsLoader)return;
   window.__vedatorEpisodeTranslationsLoader=true;
 
-  const VERSION='20260802-2030';
+  const VERSION='20260802-2035';
   const SOURCES=[
     ['episode-translations-346-337.js','data-vedator-episode-translations-346-337'],
     ['episode-translations-336-330.js','data-vedator-episode-translations-336-330'],
@@ -42,7 +42,8 @@
     ['episode-translations-67-60.js','data-vedator-episode-translations-67-60'],
     ['episode-translations-59-52.js','data-vedator-episode-translations-59-52'],
     ['episode-translations-51-44.js','data-vedator-episode-translations-51-44'],
-    ['episode-translations-43-36.js','data-vedator-episode-translations-43-36']
+    ['episode-translations-43-36.js','data-vedator-episode-translations-43-36'],
+    ['episode-translations-35-28.js','data-vedator-episode-translations-35-28']
   ];
 
   function waitForLanguageBatchController(timeout=8000){
