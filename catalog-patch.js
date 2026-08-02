41ba176a2b4e93fdb2629f93b3e2d124f18fb95b
Bernio <jendabernard@gmail.com>
2026-08-02 08:17:23 +0200

    Refresh episode translation loader for episodes 195 to 188

--- catalog-patch.js
+++ catalog-patch.js
@@ -18,7 +18,7 @@
     window.__vedatorEpisodeTranslationBootstrap=true;
     if(document.querySelector('script[data-vedator-episode-translation-bootstrap]'))return;
     const script=document.createElement('script');
-    script.src='./episode-translations-loader.js?v=20260802-0114';
+    script.src='./episode-translations-loader.js?v=20260802-0813';
     script.async=false;
     script.dataset.vedatorEpisodeTranslationBootstrap='1';
     document.head.appendChild(script);
