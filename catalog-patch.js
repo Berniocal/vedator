591063505f8f7f1fba203995e30b1a763416e3c2
Bernio <jendabernard@gmail.com>
2026-08-02 20:17:23 +0200

    Refresh translation loader for episodes 59 to 52

--- catalog-patch.js
+++ catalog-patch.js
@@ -18,7 +18,7 @@
     window.__vedatorEpisodeTranslationBootstrap=true;
     if(document.querySelector('script[data-vedator-episode-translation-bootstrap]'))return;
     const script=document.createElement('script');
-    script.src='./episode-translations-loader.js?v=20260802-2005';
+    script.src='./episode-translations-loader.js?v=20260802-2012';
     script.async=false;
     script.dataset.vedatorEpisodeTranslationBootstrap='1';
     document.head.appendChild(script);
