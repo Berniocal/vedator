653d98f0000ce54d37b9561dfdc0baf70ad73551
Bernio <jendabernard@gmail.com>
2026-08-02 10:01:30 +0200

    Load Czech translations for episodes 171 to 164

--- episode-translations-loader.js
+++ episode-translations-loader.js
@@ -2,7 +2,7 @@
   if(window.__vedatorEpisodeTranslationsLoader)return;
   window.__vedatorEpisodeTranslationsLoader=true;
 
-  const VERSION='20260802-0952';
+  const VERSION='20260802-0959';
   const SOURCES=[
     ['episode-translations-346-337.js','data-vedator-episode-translations-346-337'],
     ['episode-translations-336-330.js','data-vedator-episode-translations-336-330'],
@@ -25,7 +25,8 @@
     ['episode-translations-203-196.js','data-vedator-episode-translations-203-196'],
     ['episode-translations-195-188.js','data-vedator-episode-translations-195-188'],
     ['episode-translations-187-180.js','data-vedator-episode-translations-187-180'],
-    ['episode-translations-179-172.js','data-vedator-episode-translations-179-172']
+    ['episode-translations-179-172.js','data-vedator-episode-translations-179-172'],
+    ['episode-translations-171-164.js','data-vedator-episode-translations-171-164']
   ];
 
   const normalizeLanguage=value=>{
