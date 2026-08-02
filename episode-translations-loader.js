81563cd9b3d15a5ee03757208d0e1d0f2ce5cc8f
Bernio <jendabernard@gmail.com>
2026-08-02 21:04:32 +0200

    Load translations for episodes 19 to 12

--- episode-translations-loader.js
+++ episode-translations-loader.js
@@ -2,7 +2,7 @@
   if(window.__vedatorEpisodeTranslationsLoader)return;
   window.__vedatorEpisodeTranslationsLoader=true;
 
-  const VERSION='20260802-2051';
+  const VERSION='20260802-2101';
   const SOURCES=[
     ['episode-translations-346-337.js','data-vedator-episode-translations-346-337'],
     ['episode-translations-336-330.js','data-vedator-episode-translations-336-330'],
@@ -44,7 +44,8 @@
     ['episode-translations-51-44.js','data-vedator-episode-translations-51-44'],
     ['episode-translations-43-36.js','data-vedator-episode-translations-43-36'],
     ['episode-translations-35-28.js','data-vedator-episode-translations-35-28'],
-    ['episode-translations-27-20.js','data-vedator-episode-translations-27-20']
+    ['episode-translations-27-20.js','data-vedator-episode-translations-27-20'],
+    ['episode-translations-19-12.js','data-vedator-episode-translations-19-12']
   ];
 
   function waitForLanguageBatchController(timeout=8000){
