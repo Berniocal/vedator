376f02505a5da894ea715fe6240fe80c92540082
Bernio <jendabernard@gmail.com>
2026-08-02 20:04:36 +0200

    Load Czech translations for episodes 67 to 60

--- episode-translations-loader.js
+++ episode-translations-loader.js
@@ -2,7 +2,7 @@
   if(window.__vedatorEpisodeTranslationsLoader)return;
   window.__vedatorEpisodeTranslationsLoader=true;
 
-  const VERSION='20260802-1945';
+  const VERSION='20260802-2005';
   const SOURCES=[
     ['episode-translations-346-337.js','data-vedator-episode-translations-346-337'],
     ['episode-translations-336-330.js','data-vedator-episode-translations-336-330'],
@@ -38,7 +38,8 @@
     ['episode-translations-99-92.js','data-vedator-episode-translations-99-92'],
     ['episode-translations-91-84.js','data-vedator-episode-translations-91-84'],
     ['episode-translations-83-76.js','data-vedator-episode-translations-83-76'],
-    ['episode-translations-75-68.js','data-vedator-episode-translations-75-68']
+    ['episode-translations-75-68.js','data-vedator-episode-translations-75-68'],
+    ['episode-translations-67-60.js','data-vedator-episode-translations-67-60']
   ];
 
   function waitForLanguageBatchController(timeout=8000){
