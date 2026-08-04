88b1385d57766f7725df956ba9df02993626983f
Bernio <jendabernard@gmail.com>
2026-08-04 13:53:45 +0200

    Add fast service worker bootstrap

---
+++ sw-fast.js
@@ -0,0 +1,18 @@
+(()=>{
+  const originalAddAll=typeof Cache!=='undefined'?Cache.prototype.addAll:null;
+  if(originalAddAll){
+    const CORE_FILES=new Set(['index.html','manifest.webmanifest','icon.svg']);
+    Cache.prototype.addAll=function(requests){
+      const core=[...(requests||[])].filter(request=>{
+        try{
+          const raw=typeof request==='string'?request:request.url;
+          const url=new URL(raw,self.location.href);
+          const name=url.pathname.split('/').pop();
+          return url.pathname.endsWith('/vedator/')||CORE_FILES.has(name);
+        }catch{return false}
+      });
+      return originalAddAll.call(this,core);
+    };
+  }
+  importScripts('./sw.js');
+})();
