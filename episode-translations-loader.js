1498e390f5632811c10fe02a3ba7eedef25c4df7
Bernio <jendabernard@gmail.com>
2026-08-08 15:32:52 +0200

    Load episode 343 summary

--- episode-translations-loader.js
+++ episode-translations-loader.js
@@ -3,9 +3,10 @@
   window.__vedatorEpisodeTranslationsLoader=true;
   window.__vedatorEpisodeTranslationsReady=false;
 
-  const VERSION='20260806-episode-347';
+  const VERSION='20260808-episode-343-summary';
   const SOURCES=[
     ['episode-translations-347.js','data-vedator-episode-translations-347'],
+    ['episode-343-summary.js','data-vedator-episode-343-summary'],
     ['episode-translations-346-337.js','data-vedator-episode-translations-346-337'],
     ['episode-translations-336-330.js','data-vedator-episode-translations-336-330'],
     ['episode-translations-329-323.js','data-vedator-episode-translations-329-323'],
