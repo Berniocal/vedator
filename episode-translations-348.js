edcd97bdc02842db2ac2b19c8ace19cbcffc108b
Bernio <jendabernard@gmail.com>
2026-08-20 09:05:16 +0200

    Přidat český překlad epizody 348 bezpečnou V2 cestou
    * Přidat český překlad epizody 348
    
    * Spouštět bezpečný rebuild i po změně překladů epizod
    
    * Ověřovat překlady epizod v PR testech

---
+++ episode-translations-348.js
@@ -0,0 +1,21 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation348)return;
+  window.__vedatorEpisodeTranslation348=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 348 – Teleskop Very Rubin',
+    csTitle:'Vedátorský podcast 348 – Teleskop Very Rubin',
+    skLead:'Ako funguje najväčší digitálny fotoaparát na svete? Dá sa z nočnej oblohy vytvoriť desaťročný film? Dokážeme zachytiť zmeny vo vesmíre v priebehu pár minút? Odhalí nové observatórium tajomstvá tmavej hmoty a energie? O tom všetkom diskutujú Jozef, Samuel a ich hosťka Danica Žilková.',
+    csLead:'Jak funguje největší digitální fotoaparát na světě? Dá se z noční oblohy vytvořit desetiletý film? Dokážeme zachytit změny ve vesmíru během několika minut? Odhalí nová observatoř tajemství temné hmoty a energie? O tom všem diskutují Jozef, Samuel a jejich hostka Danica Žilková.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[
+    ['Podcast vzniká v spolupráci so SME.','Podcast vzniká ve spolupráci se SME.'],
+    ['Registrujte sa na živé nahrávanie:','Zaregistrujte se na živé nahrávání:'],
+    ['Bonusové epizódy a extra obsah k podcastom nájdete na','Bonusové epizody a další obsah k podcastům najdete na'],
+    ['Samuelova nová kniha už je v predaji','Samuelova nová kniha je už v prodeji'],
+    ['Otázky nám môžete nahrávať tu','Otázky nám můžete nahrávat zde'],
+    ['Podcastové hrnčeky a ponožky nájdete na stránke','Podcastové hrnky a ponožky najdete na stránce'],
+    ['Všetko ostatné nájdete tu','Všechno ostatní najdete zde']
+  ];
+})();
