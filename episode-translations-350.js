340123ce1b1b713060d17b5e9c7323142760d917
Bernio <jendabernard@gmail.com>
2026-08-27 07:19:19 +0200

    Přidat epizodu 350 a český překlad bezpečnou V2 cestou

---
+++ episode-translations-350.js
@@ -0,0 +1,20 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation350)return;
+  window.__vedatorEpisodeTranslation350=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 350 – FAQ detské otázky',
+    csTitle:'Vedátorský podcast 350 – FAQ dětské otázky',
+    skLead:'Ako presne vzniká pestrofarebná dúha na oblohe? Prečo nám pri hraní hier uteká čas oveľa rýchlejšie? Kto v skutočnosti posolil všetku vodu v moriach? Kedy nastane deň, keď čierna diera zožerie naše Slnko? O tom všetkom diskutujú Jozef a Samuel.',
+    csLead:'Jak přesně vzniká pestrobarevná duha na obloze? Proč nám při hraní her utíká čas mnohem rychleji? Kdo ve skutečnosti osolil všechnu vodu v mořích? Kdy nastane den, kdy černá díra sežere naše Slunce? O tom všem diskutují Jozef a Samuel.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[
+    ['Podcast vzniká v spolupráci so SME.','Podcast vzniká ve spolupráci se SME.'],
+    ['Bonusové epizódy a extra obsah k podcastom nájdete na','Bonusové epizody a další obsah k podcastům najdete na'],
+    ['Samuelova nová kniha už je v predaji','Samuelova nová kniha je už v prodeji'],
+    ['Otázky nám môžete nahrávať tu','Otázky nám můžete nahrávat zde'],
+    ['Podcastové hrnčeky a ponožky nájdete na stránke','Podcastové hrnky a ponožky najdete na stránce'],
+    ['Všetko ostatné nájdete tu','Všechno ostatní najdete zde']
+  ];
+})();
