1164a7677c4dbad2e82abbbd2b2c75f1fb3e8bbb
Bernio <jendabernard@gmail.com>
2026-09-10 05:01:54 +0200

    Add Czech translation for episode 353

---
+++ episode-translations-353.js
@@ -0,0 +1,13 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation353)return;
+  window.__vedatorEpisodeTranslation353=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 353 – Moderné CT',
+    csTitle:'Vedátorský podcast 353 – Moderní CT',
+    skLead:'Ako dokáže CT prístroj zobraziť vnútro ľudského tela bez jediného rezu? Prečo sa obrovská konštrukcia v skeneri musí točiť rýchlosťou až štyrikrát za sekundu? Dokážeme vďaka novej technológii detailne zobraziť aj najmenšiu kostičku v ľudskom tele? O tom všetkom diskutujú Jozef, Samuel a Igor Marga.',
+    csLead:'Jak dokáže CT přístroj zobrazit vnitřek lidského těla bez jediného řezu? Proč se obrovská konstrukce ve skeneru musí otáčet rychlostí až čtyřikrát za sekundu? Dokážeme díky nové technologii detailně zobrazit i nejmenší kůstku v lidském těle? O tom všem diskutují Jozef, Samuel a Igor Marga.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[];
+})();
