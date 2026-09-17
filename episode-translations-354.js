cb86e1c84c845e625e20e534a856f5f176b51996
Bernio <jendabernard@gmail.com>
2026-09-17 08:14:09 +0200

    Přidat český překlad epizody 354

---
+++ episode-translations-354.js
@@ -0,0 +1,13 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation354)return;
+  window.__vedatorEpisodeTranslation354=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 354 – Kofeín',
+    csTitle:'Vedátorský podcast 354 – Kofein',
+    skLead:'Ako kofeín v našom tele blokuje adenozínové receptory a potláča pocity únavy? Prečo si rastliny vyvinuli kofeín ako chemickú zbraň aj ako lákadlo pre včely? Aké účinky má kofeín na fyzický výkon športovcov a mobilizáciu tukových zásob? O tom všetkom diskutujú Jozef, Samuel a ich hosťka Lenka „Skúmavka" Hrobárová.',
+    csLead:'Jak kofein v našem těle blokuje adenosinové receptory a potlačuje pocity únavy? Proč si rostliny vyvinuly kofein jako chemickou zbraň i jako lákadlo pro včely? Jaké účinky má kofein na fyzický výkon sportovců a mobilizaci tukových zásob? O tom všem diskutují Jozef, Samuel a jejich hostka Lenka „Skúmavka" Hrobárová.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[];
+})();
