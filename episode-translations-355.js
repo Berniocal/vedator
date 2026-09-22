af288d62a3607892971792c7f7442e2e3860e005
Bernio <jendabernard@gmail.com>
2026-09-22 05:02:37 +0200

    translation: add Czech episode 355

---
+++ episode-translations-355.js
@@ -0,0 +1,13 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation355)return;
+  window.__vedatorEpisodeTranslation355=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 355 – Vesmírna kolíska',
+    csTitle:'Vedátorský podcast 355 – Vesmírná kolébka',
+    skLead:'Ako vzniká kniha o vesmíre počas nočného tíšenia novorodenca? Prečo by hviezdy podľa fyzikálnych zákonov vlastne ani nemali fungovať? Ako dokáže nočné pozorovanie oblohy a obyčajné kráčanie upokojiť myseľ? Čo všetko spája hľadanie mimozemského života, asteroidy a vesmírne vzdialenosti? O tom všetkom diskutujú Jozef a Samuel.',
+    csLead:'Jak vzniká kniha o vesmíru během nočního tišení novorozence? Proč by hvězdy podle fyzikálních zákonů vlastně ani neměly fungovat? Jak dokáže noční pozorování oblohy a obyčejná chůze uklidnit mysl? Co všechno spojuje hledání mimozemského života, asteroidy a vesmírné vzdálenosti? O tom všem diskutují Jozef a Samuel.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[];
+})();
