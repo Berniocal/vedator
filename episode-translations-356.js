78da44a291c942ad45e991e8091dbb54c3802fb9
Bernio <jendabernard@gmail.com>
2026-09-24 05:04:00 +0200

    Přidat český překlad epizody 356

---
+++ episode-translations-356.js
@@ -0,0 +1,13 @@
+(()=>{
+  if(window.__vedatorEpisodeTranslation356)return;
+  window.__vedatorEpisodeTranslation356=true;
+
+  const TRANSLATION={
+    skTitle:'Vedátorský podcast 356 – Miléniové problémy 3: Navier-Stokesove rovnice',
+    csTitle:'Vedátorský podcast 356 – Miléniové problémy 3: Navier-Stokesovy rovnice',
+    skLead:'Čo sú to miléniové problémy a prečo je za ich vyriešenie odmena milión dolárov? Prečo robia rovnice opisujúce prúdenie tekutín fyzikom nočné mory? Dá sa z vírov a prúdenia vody skonštruovať fungujúci počítač? O tom všetkom diskutujú Jozef a Samuel.',
+    csLead:'Co jsou miléniové problémy a proč je za jejich vyřešení odměna milion dolarů? Proč způsobují rovnice popisující proudění tekutin fyzikům noční můry? Dá se z vírů a proudění vody sestrojit fungující počítač? O tom všem diskutují Jozef a Samuel.'
+  };
+
+  const DESCRIPTION_TEXT_PAIRS=[];
+})();
